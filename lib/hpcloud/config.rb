ae44f180513c6cae9133bec8ca8d0254c7d40c3c
Matt Sanders <matt@modal.org>
2011-03-30 14:56:19 -0500

    Add Config class with basic functions for config setup and account file management.

---
+++ lib/hpcloud/config.rb
@@ -0,0 +1,42 @@
+require 'yaml'
+
+module HPCloud
+  class Config
+
+    def self.config_directory
+      home_directory + "/.hpcloud/"
+    end
+    
+    def self.home_directory
+      @@home_dir ||= ENV['HOME']
+    end
+    
+    def self.home_directory=(dir)
+      @@home_dir = dir # allow overload for testing
+    end
+    
+    def self.accounts_directory
+      home_directory + 'accounts/'
+    end
+    
+    def self.update_credentials(account, credentials)
+      ensure_config_exists
+      write_account account, credentials
+    end
+    
+    # Note: May want to port some of this to Thor's native actions eventually?
+    def self.ensure_config_exists
+      Dir.mkdir(config_directory) unless Dir.exists?(config_directory)
+      Dir.mkdir(accounts_directory) unless Dir.exists?(accounts_directory)
+      # TODO: write default config file if not present
+    end
+    
+    def self.write_account(account_name, credentials)
+      contents = {:credentials => credentials}
+      File.open("#{accounts_directory}#{account_name.to_s.downcase.gsub(' ', '_')}", 'w') do |file|
+        file.write contents.to_yaml
+      end
+    end
+    
+  end
+end
---
+++ spec/unit/config_spec.rb
@@ -0,0 +1,86 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'fileutils'
+require 'yaml'
+
+describe "Config directory naming" do
+  it "should assemble properly" do
+    HPCloud::Config.config_directory.should eql(ENV['HOME'] + '/.hpcloud/')
+  end
+  
+  it "should include final slash" do
+    HPCloud::Config.config_directory[-1].should eql('/')
+  end
+end
+
+describe "Config directory setup" do
+  
+  before(:all) { setup_temp_home_directory } 
+
+  context "with no config directory present" do
+    
+    before(:all) do
+      FileUtils.rm_rf(HPCloud::Config.config_directory)
+    end
+    
+    context "running ensure config" do
+      
+      before(:all) { HPCloud::Config.ensure_config_exists }
+      
+      it "should create base config directory" do
+        Dir.exists?(HPCloud::Config.config_directory).should be_true
+      end
+      
+      it "should create accounts directory" do
+        Dir.exists?(HPCloud::Config.accounts_directory).should be_true
+      end
+      
+    end
+    
+  end
+  
+end
+
+describe "Writing an account file" do
+  
+  before(:all) do
+    setup_temp_home_directory
+    HPCloud::Config.ensure_config_exists
+  end
+  
+  context "when account does not exist yet" do
+    
+    context "(default account)" do
+
+      before(:all) do
+        credentials = {:email => 'test@test.com', :account_id => '1234', :secret_key => 'foo'}
+        HPCloud::Config.write_account(:default, credentials)
+      end
+
+      it "should create a file using account name" do
+        File.exists?(HPCloud::Config.accounts_directory + 'default')
+      end
+      
+      it "should have nested credentials" do
+        yaml = YAML::load(File.open(HPCloud::Config.accounts_directory + 'default'))
+        yaml.should have_key(:credentials)
+      end
+      
+      it "should have written credential fields" do
+        yaml = YAML::load(File.open(HPCloud::Config.accounts_directory + 'default'))
+        yaml[:credentials][:email].should eql('test@test.com')
+        yaml[:credentials][:account_id].should eql('1234')
+        yaml[:credentials][:secret_key].should eql('foo')
+      end
+
+    end
+    
+  end
+  
+end
+
+private
+
+def setup_temp_home_directory
+  HPCloud::Config.home_directory = File.expand_path(File.dirname(__FILE__) + '/../tmp/home')
+  Dir.mkdir(HPCloud::Config.home_directory) unless Dir.exists?(HPCloud::Config.home_directory)
+end
