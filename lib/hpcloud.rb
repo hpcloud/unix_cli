cf20f3596481cb2cef85490da722c5818f6d5bf6
Matt Sanders <matt@modal.org>
2011-03-31 09:57:26 -0500

    Use credentials from account file for all operations.

--- lib/hpcloud.rb
+++ lib/hpcloud.rb
@@ -11,23 +11,20 @@ require 'hpcloud/bucket'
 module HPCloud
   class CLI < Thor
     
-    # export KVS_TEST_HOST=16.49.184.31
-    # build/opt-centos5-x86_64/bin/stout-mgr create-account -port 9232 "Unix CLI" "unix.cli@hp.com"
-    
-    ACCESS_ID = '9cb90f0dcb0fe61380b971e68ed4efb11beb6bfc'
-    SECRET_KEY = 'b04517f6189f3695b9804d69cd6925715a531be5'
-    ACCOUNT_ID = '7725f6e048ceb766a546842adc07d22a22c5f930'
-    HOST = '16.49.184.31'
-    PORT = '9232'
-    
     private
     
     def connection
-      @connection ||= Fog::HP::Storage.new( :hp_access_id =>  ACCESS_ID,
-                                            :hp_secret_key => SECRET_KEY,
-                                            :hp_account_id => ACCOUNT_ID,
-                                            :host => HOST,
-                                            :port => PORT )
+      return @connection if @connection
+      credentials = Config.current_credentials
+      if credentials
+        @connection ||= Fog::HP::Storage.new( :hp_access_id =>  credentials[:access_id],
+                                              :hp_secret_key => credentials[:secret_key],
+                                              :hp_account_id => credentials[:email],
+                                              :host => credentials[:host],
+                                              :port => credentials[:port] )
+      else
+        error "Please run `hpcloud account:setup` to set up your account."
+      end
     end
     
     def display_error_message(error)
