395f786c79405a999fdee6360b010762da60508c
Alex Vorbau <alex.vorbau@hp.com>
2011-04-06 11:52:33 -0700

    Now only asks for confirmation for bucket remove. Switched to using Fog model objects instead of request commands. Changed wording of error message. No longer print colon in bucket names in error messages.

--- lib/hpcloud/commands/remove.rb
+++ lib/hpcloud/commands/remove.rb
@@ -1,38 +1,39 @@
 module HPCloud
   class CLI < Thor
-    
+
     map %w(rm delete destroy del) => 'remove'
-    
+
     desc 'remove <resource>', 'remove an object'
     def remove(resource)
       bucket, path = Bucket.parse_resource(resource)
       type = Resource.detect_type(resource)
 
-      confirm = ask_with_default "Are you sure you want to remove '#{resource}'", 'n'
-      if confirm[0].downcase == 'y'
-        begin
-          if type == :object
-            # This test for existance will raise a NotFound and print an error
-            # We do this because the call to delete_object does not error out if resource doens't exist
-            connection.head_object(bucket, path)
-            connection.delete_object(bucket, path)
-            puts "Removed object #{resource}."
-          elsif type == :bucket
-            connection.head_bucket(bucket)
-            send('buckets:remove', bucket)
-          else
-            error "I don't understand the resource '#{resource}'"
-          end
-        rescue Excon::Errors::NotFound => e
-          if connection.directories.get(bucket)
-            error "The specified object does not exist."
-            return
-          else
-            error "You don't have a bucket '#{bucket}'."
-            return
-          end
+      directory = connection.directories.get(bucket)
+      if not directory
+        error "You don't have a bucket named '#{bucket}'"
+        return
+      end
+
+      if type == :object
+        file = directory.files.get(path)
+        if file
+          file.destroy
+          puts "Removed object '#{resource}'."
+        else
+          error "You don't have a object named '#{path}'."
         end
+
+      elsif type == :bucket
+        confirm = ask_with_default "Are you sure you want to remove '#{resource}'", 'n'
+        if confirm[0].downcase == 'y'
+          send('buckets:remove', bucket)
+        end
+
+      else
+          error "Could not find resource '#{resource}'. Correct syntax is :bucketname/objectname."
       end
     end
+
   end
 end
+
