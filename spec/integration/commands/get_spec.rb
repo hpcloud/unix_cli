565c07fa21d0dadef227ce68e077b6193865cdbb
Matt Sanders <matt@modal.org>
2011-04-08 17:32:34 -0500

    No commas in spec names to keep CI reporter happy.

--- spec/integration/commands/get_spec.rb
+++ spec/integration/commands/get_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "Get command," do
+describe "Get command" do
   
   before(:all) do
     @kvs = storage_connection
