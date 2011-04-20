557f19baf53d940410836d6d27d909e46b4d7fa7
Alex Vorbau <alex.vorbau@hp.com>
2011-04-19 23:24:06 -0700

    More help changes.

--- lib/scalene/commands/touch.rb
+++ lib/scalene/commands/touch.rb
@@ -6,7 +6,7 @@ module HP
       long_desc "Create an empty, zero-sized object (file)
                 \n\nExamples:
                 \n\nscalene touch :my_bucket/an_empty_file.txt ==> Create a new file called 'my_file.txt'
-                \n\nAliases: none
+                \n\nAliases: none"
 
       def touch(resource)
         display "touch an object"
--- lib/scalene/commands/versioning.rb
+++ lib/scalene/commands/versioning.rb
@@ -6,7 +6,7 @@ module HP
       long_desc "Show or set the versioning state for a bucket.
                 \n\nExamples:
                 \n\nscalene versioning :my_bucket
-                \n\nAliases: none
+                \n\nAliases: none"
       def versioning(new_state=nil)
         display "show versioning state"
       end
