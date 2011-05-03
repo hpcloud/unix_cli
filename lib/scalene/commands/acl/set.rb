c00c252c338e01ebb3efffa935e4da7c0c9f16ba
Matt Sanders <matt@modal.org>
2011-05-03 09:31:57 -0500

    Update long description for acl:set command.

--- lib/scalene/commands/acl/set.rb
+++ lib/scalene/commands/acl/set.rb
@@ -2,17 +2,23 @@ module HP
   module Scalene
     class CLI < Thor
     
-      desc 'acl:set <resource> <canned-acl>', "set a given resource to a canned ACL"
-      long_desc "Set the Access Control List (ACL) for the specified resource, either a bucket or object.
-                 The supported ACL options include: private, public-read, public-read-write, authenticated-read,
-                 authenticated-read-write, bucket-owner-read, bucket-owner-full-control, log-delivery-write.
-                \n\nExamples:
-                \n\nscalene acl:set :my_bucket/my_file.txt public-read ==> Set the ACL for the file 'my_file.txt' to 'public-read'
-                \n\nscalene acl:set :my_bucket private ==> Set the ACL for the bucket ':my_bucket' to 'private'
+      desc 'acl:set <resource> <acl>', "set a given resource to a canned ACL"
+      long_desc <<-DESC
+  Set the Access Control List (ACL) for the specified resource, either a 
+  bucket or object.
+                 
+  The supported ACL options include: private, public-read, public-read-write, 
+  authenticated-read, authenticated-read-write, bucket-owner-read, 
+  bucket-owner-full-control, log-delivery-write.
 
-                \n\nAliases: none
-                \n\nNote: For an explanation of the canned ACLs, visit http://..."
+Examples:
+  scalene acl:set :my_bucket/file public-read  # Set 'file' ACL to public-read
+  scalene acl:set :my_bucket private           # Set 'my_bucket' ACL private
 
+Aliases: none
+
+Note: Custom ACLs will be supported in a future release.
+      DESC
       define_method 'acl:set' do |resource, acl|
         acl = acl.downcase
         unless CANNED_ACLS.include?(acl)
