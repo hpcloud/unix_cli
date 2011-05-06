2da4a65529d9e48c1377b779757d8b592fe50452
Matt Sanders <matt@modal.org>
2011-05-06 12:33:28 -0500

    Split acl and acl:set specs into separate files.

--- spec/integration/commands/acl_spec.rb
+++ spec/integration/commands/acl_spec.rb
@@ -1,79 +1,53 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "Acl commands" do
-
+describe "Acl command (viewing acls)" do
+  
   before(:all) do
     @kvs = storage_connection
     @kvs.put_bucket('acl_bucket')
     @kvs.put_object('acl_bucket', 'foo.txt', read_file('foo.txt'))
   end
-
-  context "Acl:set command (setting acls)" do
-    context "when resource is not correct" do
-      it "should exit with message about not supported resource" do
-        response = capture(:stderr){ HP::Scalene::CLI.start(['acl:set', '/foo/foo', 'private']) }
-        response.should eql("Setting ACLs is only supported for buckets and objects\n")
-      end
-    end
-    context "when acl string is not correct" do
-      it "should exit with message about bad acl" do
-        response = capture(:stderr){ HP::Scalene::CLI.start(['acl:set', ':foo_bucket', 'blah-acl']) }
-        response.should eql("Your ACL 'blah-acl' is invalid.\nValid options are: private, public-read, public-read-write, authenticated-read, authenticated-read-write, bucket-owner-read, bucket-owner-full-control, log-delivery-write.\n")
-      end
-    end
-    context "when setting the ACL for a bucket" do
-      it "should report success" do
-        response = capture(:stdout){ HP::Scalene::CLI.start(['acl:set', ':acl_bucket', 'authenticated-read']) }
-        response.should eql("ACL for :acl_bucket updated to authenticated-read\n")
-      end
-    end
-    context "when setting the ACL for an object" do
-      it "should report success" do
-        response = capture(:stdout){ HP::Scalene::CLI.start(['acl:set', ':acl_bucket/foo.txt', 'authenticated-read']) }
-        response.should eql("ACL for :acl_bucket/foo.txt updated to authenticated-read\n")
-      end
+  
+  context "when resource is not correct" do
+    it "should exit with message about not supported resource" do
+      response = capture(:stderr){ HP::Scalene::CLI.start(['acl', '/foo/foo']) }
+      response.should eql("ACL viewing is only supported for buckets and objects\n")
     end
   end
-  context "Acl command (viewing acls)" do
-    context "when resource is not correct" do
-      it "should exit with message about not supported resource" do
-        response = capture(:stderr){ HP::Scalene::CLI.start(['acl', '/foo/foo']) }
-        response.should eql("ACL viewing is only supported for buckets and objects\n")
-      end
+  
+  context "when viewing the ACL for a bucket" do
+    before (:all) do
+      @kvs.put_bucket_acl('acl_bucket', 'authenticated-read-write')
+      @response = capture(:stdout){ HP::Scalene::CLI.start(['acl', ':acl_bucket']) }
     end
-    context "when viewing the ACL for a bucket" do
-      before (:all) do
-        @kvs.put_bucket_acl('acl_bucket', 'authenticated-read-write')
-        @response = capture(:stdout){ HP::Scalene::CLI.start(['acl', ':acl_bucket']) }
-      end
-      it "should have FULL_CONTROL permissions" do
-        @response.should include("FULL_CONTROL")
-      end
-      it "should have READ permissions for AuthenticatedUsers group" do
-        @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  READ")
-      end
-      it "should have WRITE permissions for AuthenticatedUsers group" do
-        @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  WRITE")
-      end
+    it "should have FULL_CONTROL permissions" do
+      @response.should include("FULL_CONTROL")
     end
-    context "when viewing the ACL for an object" do
-      before (:all) do
-        @kvs.put_object_acl('acl_bucket', 'foo.txt','authenticated-read-write')
-        @response = capture(:stdout){ HP::Scalene::CLI.start(['acl', ':acl_bucket/foo.txt']) }
-      end
-      it "should have FULL_CONTROL permissions" do
-        @response.should include("FULL_CONTROL")
-      end
-      it "should have READ permissions for AuthenticatedUsers group" do
-        @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  READ")
-      end
-      it "should have WRITE permissions for AuthenticatedUsers group" do
-        @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  WRITE")
-      end
+    it "should have READ permissions for AuthenticatedUsers group" do
+      @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  READ")
+    end
+    it "should have WRITE permissions for AuthenticatedUsers group" do
+      @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  WRITE")
     end
   end
-
+  
+  context "when viewing the ACL for an object" do
+    before (:all) do
+      @kvs.put_object_acl('acl_bucket', 'foo.txt','authenticated-read-write')
+      @response = capture(:stdout){ HP::Scalene::CLI.start(['acl', ':acl_bucket/foo.txt']) }
+    end
+    it "should have FULL_CONTROL permissions" do
+      @response.should include("FULL_CONTROL")
+    end
+    it "should have READ permissions for AuthenticatedUsers group" do
+      @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  READ")
+    end
+    it "should have WRITE permissions for AuthenticatedUsers group" do
+      @response.should include("http://acs.amazonaws.com/groups/global/AuthenticatedUsers  WRITE")
+    end
+  end
+  
   after(:all) do
      purge_bucket('acl_bucket')
-   end
+  end
 end
