2410825e8488788bb612d8e2b167930c571810b3
Matt Sanders <matt@modal.org>
2011-05-16 14:02:08 -0500

    Add new shorter syntax for running commands.

--- spec/helpers/io.rb
+++ spec/helpers/io.rb
@@ -1,6 +1,11 @@
 
 RSpec.configure do |config|
   
+  def cli_command(string)
+    name, *args = string.split(' ')
+    CLITester.new(name, args)
+  end
+  
   # Capture a certain io stream for anything run within the block
   #
   # Example:  output = capture('stdout') { puts 'hello world' }
@@ -48,5 +53,68 @@ RSpec.configure do |config|
       message
     end
   end
+  
+end
+
+class CLITester
+  
+  def initialize(command_name, args=nil)
+    @command_name = command_name.to_s
+    @args = args || []
+  end
+  
+  # def with(arguments)
+  #   @args = arguments.to_s
+  #   return self
+  # end
+  
+  def stdout
+    capture(:stdout){ HP::Scalene::CLI.start([@command_name, *@args]) }
+  end
+  
+  def stderr
+    capture(:stderr){ HP::Scalene::CLI.start([@command_name, *@args]) }
+  end
+  
+  def stderr_and_exit_status
+    capture_with_status(:stderr){ HP::Scalene::CLI.start([@command_name, *@args]) }
+  end
+  
+  def to_s
+    @cached_string ||= self.stdout
+  end
+  
+  def include?(string)
+    self.to_s.include?(string)
+  end
+  
+  private
+  
+    # Capture a certain io stream for anything run within the block
+    #
+    # Example:  output = capture('stdout') { puts 'hello world' }
+    def capture(stream)
+      capture_with_status(stream){ yield }[0]
+    end
+    
+    # Capture both io stream and exit status
+    #
+    # Example:  output, exit_status = capture('stdout') { puts 'hello world' }
+    def capture_with_status(stream)
+      exit_status = 0
+      begin
+        stream = stream.to_s
+        eval "$#{stream} = StringIO.new"
+        begin
+          yield
+        rescue SystemExit => system_exit # catch any exit calls
+          exit_status = system_exit.status
+        end
+        result = eval("$#{stream}").string
+      ensure
+        eval("$#{stream} = #{stream.upcase}")
+      end
+      return result, exit_status
+    end
   
 end
