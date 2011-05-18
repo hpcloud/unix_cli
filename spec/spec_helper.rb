97260158762e322996d050c17b6cca8b69b0b4ba
Rupak Ganguly <rupakg@gmail.com>
2011-05-18 16:02:59 -0400

    Add constant account id for use with mocks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,15 +28,17 @@ RSpec.configure do |config|
   # build/opt-centos5-x86_64/bin/stout-mgr create-account -port 9233 "Unix CLI" "unix.cli@hp.com"
   #
   # http://16.49.184.32:9242/kvs/keygen.html
+  MOCKING_ENABLED = ENV['ENABLE_CLI_MOCKING'] || false
+  MOCK_ACCOUNT_ID = '2744ccd10c7533bd736ad890f9dd5cab2adb27b07d500b9493f29cdc420cb2e0'
+
   KVS_ACCESS_ID = '0fe012e5db618a083e6386464718f1e4327a1918'
   KVS_SECRET_KEY = '11e36dbbcb2d9a17397d1df3f37f7f4e7275b39d'
-  KVS_ACCOUNT_ID = '145197220011'
+  KVS_ACCOUNT_ID = !MOCKING_ENABLED ? '145197220011' : MOCK_ACCOUNT_ID
   KVS_HOST = '16.49.184.32'
   KVS_PORT = '9242'
   SEC_KVS_ACCESS_ID = '92adebfc85cfc2800ea7bea7ae16045c276c4ec2'
   SEC_KVS_SECRET_KEY = '682c572b82d86071919d86e6c12f43d067da92fb'
-  SEC_KVS_ACCOUNT_ID = '529285467718'
-  MOCKING_ENABLED = ENV['ENABLE_CLI_MOCKING'] || false
+  SEC_KVS_ACCOUNT_ID = !MOCKING_ENABLED ? '529285467718' : MOCK_ACCOUNT_ID
 
   if MOCKING_ENABLED
     puts "==========================================================="
