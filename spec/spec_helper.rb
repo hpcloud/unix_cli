73c74560a6faf4119d0e9fc0a59cc8739d01bff1
Rupak Ganguly <rupakg@gmail.com>
2011-08-11 17:19:07 -0400

    minor change to a message.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,10 +24,6 @@ require 'helpers/containers'
 
 RSpec.configure do |config|
   
-  # export KVS_TEST_HOST=16.49.184.31
-  # build/opt-centos5-x86_64/bin/stout-mgr create-account -port 9233 "Unix CLI" "unix.cli@hp.com"
-  #
-  # http://16.49.184.32:9242/kvs/keygen.html
   MOCKING_ENABLED = ENV['ENABLE_CLI_MOCKING'] || false
 
   OS_STORAGE_AUTH_URL         = ENV['OS_STORAGE_AUTH_URL']  || "http://15.184.3.19:8080/auth/v1.0"  # "http://agpa-ge1.csbu.hpl.hp.com/auth/v1.0"
@@ -35,6 +31,14 @@ RSpec.configure do |config|
   OS_STORAGE_ACCOUNT_PASSWORD = ENV['OS_STORAGE_ACCOUNT_PASSWORD']  || "pass1"             # "fog_tester1"
   OS_STORAGE_SEC_ACCOUNT_USERNAME = ENV['OS_STORAGE_SEC_ACCOUNT_USERNAME']  || "account2:user2"   # "fog_tester2:fog_tester2"
   OS_STORAGE_SEC_ACCOUNT_PASSWORD = ENV['OS_STORAGE_SEC_ACCOUNT_PASSWORD']  || "pass2"            # "fog_tester2"
+
+  OS_COMPUTE_AUTH_URL         = ENV['OS_COMPUTE_AUTH_URL']  || "http://15.184.4.165:8774/v1.0" # "http://15.9.138.42:8774/v1.1"
+  OS_COMPUTE_ACCOUNT_USERNAME = ENV['OS_COMPUTE_ACCOUNT_USERNAME']  || "rupakg" #"user10"
+  OS_COMPUTE_ACCOUNT_PASSWORD = ENV['OS_COMPUTE_ACCOUNT_PASSWORD']  || "c494f753-b6dd-4e49-b89e-72199eb36f4a" # "221a5420-bf8e-4ff9-a108-0be01689de97"
+
+  EC2_COMPUTE_AUTH_URL         = ENV['EC2_COMPUTE_AUTH_URL']  || "http://15.184.4.165:8773/services/Cloud"
+  EC2_COMPUTE_ACCOUNT_USERNAME = ENV['EC2_COMPUTE_ACCOUNT_USERNAME']  || "c494f753-b6dd-4e49-b89e-72199eb36f4a:rupakg"
+  EC2_COMPUTE_ACCOUNT_PASSWORD = ENV['EC2_COMPUTE_ACCOUNT_PASSWORD']  || "8d1081c4-02e2-49b7-b59b-6b532f2c6a03"
 
   if MOCKING_ENABLED
     puts "==========================================================="
@@ -44,8 +48,8 @@ RSpec.configure do |config|
     # Enable mocking
     Fog.mock!
   else
-#    puts "Running tests against KVS http://#{KVS_HOST}:#{KVS_PORT}..."
     puts "Running tests against HP OpenStack Storage (Swift) instance: #{OS_STORAGE_AUTH_URL}..."
+    puts "and HP OpenStack (EC2 compatibility) Compute (Nova) instance: #{EC2_COMPUTE_AUTH_URL}..."
   end
 end
 
