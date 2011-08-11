0157a7721f12fb2fcfbf610168a8e6cd39dee347
Rupak Ganguly <rupakg@gmail.com>
2011-08-11 17:20:30 -0400

    Add connection for compute service.

--- spec/helpers/connections.rb
+++ spec/helpers/connections.rb
@@ -17,7 +17,13 @@ RSpec.configure do |config|
     end
   end
   
-  
+  def compute_connection
+    Fog::Compute.new( :provider              => 'AWS',
+                      :aws_access_key_id     => EC2_COMPUTE_ACCOUNT_USERNAME,
+                      :aws_secret_access_key => EC2_COMPUTE_ACCOUNT_PASSWORD,
+                      :endpoint              => EC2_COMPUTE_AUTH_URL )
+  end
+
   
 end
 
@@ -25,16 +31,23 @@ end
 module HP::Cloud
   class CLI < Thor
   
-  private
-  
-  # override #connection not to look at account files, just use hardcoded
-  # test credentials.
-  def connection
-    Fog::Storage.new( :provider      => 'HP',
-                      :hp_account_id => OS_STORAGE_ACCOUNT_USERNAME,
-                      :hp_secret_key => OS_STORAGE_ACCOUNT_PASSWORD,
-                      :hp_auth_uri   => OS_STORAGE_AUTH_URL )
-  end
+    private
+
+    # override #connection not to look at account files, just use hardcoded
+    # test credentials.
+    def connection(service = :storage)
+      if service == :storage
+        Fog::Storage.new( :provider      => 'HP',
+                          :hp_account_id => OS_STORAGE_ACCOUNT_USERNAME,
+                          :hp_secret_key => OS_STORAGE_ACCOUNT_PASSWORD,
+                          :hp_auth_uri   => OS_STORAGE_AUTH_URL )
+      else
+        Fog::Compute.new( :provider              => 'AWS',
+                          :aws_access_key_id     => EC2_COMPUTE_ACCOUNT_USERNAME,
+                          :aws_secret_access_key => EC2_COMPUTE_ACCOUNT_PASSWORD,
+                          :endpoint              => EC2_COMPUTE_AUTH_URL )
+      end
+    end
   
   end
 end
