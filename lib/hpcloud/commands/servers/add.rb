0efb0282f9237946cd5a3d1fa55002d795fd978e
Rupak Ganguly <rupakg@gmail.com>
2011-08-17 15:43:38 -0400

    Cleanup comment code.

--- lib/hpcloud/commands/servers/add.rb
+++ lib/hpcloud/commands/servers/add.rb
@@ -15,7 +15,6 @@ Examples:
 
 Aliases: none
       DESC
-      #method_option :force, :default => false, :type => :boolean, :aliases => '-f', :desc => "Don't prompt if container name is not valid virtual host."
       define_method "servers:add" do |name, image, flavor="m1.tiny", avlzone="us-east-1a", groups=["default"]|
         # setup connection for compute service
         compute_connection = connection(:compute)
