da4c540aa10d4ccf2e0fff2bd1c84907a1cb7122
Rupak Ganguly <rupakg@gmail.com>
2011-08-17 14:12:06 -0400

    Fix specs for servers:remove command.

--- spec/integration/commands/servers/remove_spec.rb
+++ spec/integration/commands/servers/remove_spec.rb
@@ -9,24 +9,28 @@ describe "servers:remove command" do
     @hp_svc = compute_connection
   end
 
-  ### Till we can get back the server id that is created, it cannot be deleted, hence marked pending
-  pending "when deleting server with id" do
+  context "when deleting server with id" do
     before(:all) do
-      @response, @exit = run_command('servers:add fog-test-server ami-00000005').stdout_and_exit_status
-      ### Need to somehow get back the server id after it is created
-      #@new_server_id
+      response = @hp_svc.run_instances('ami-00000005', 1, 1, {'InstanceType' => 'm1.small'})
+      @new_server_id = response.body['instancesSet'][0]['instanceId']
+      @server = get_server(@hp_svc, @new_server_id)
     end
 
-    pending "should show success message" do
-      server = get_server(@hp_svc, @new_server_id)
-      server.destroy
-      @response.should include("Removed server '#{@new_server_id}'.\n")
+    it "should show success message" do
+      @response, @exit = run_command("servers:remove #{@new_server_id}").stdout_and_exit_status
+      @response.should eql("Removed server '#{@new_server_id}'.\n")
+      sleep(10)
     end
-    its_exit_status_should_be(:success)
 
+    ### server deletes take time to get it off the list
     pending "should not list in servers" do
       servers = @hp_svc.servers.map {|s| s.id}
       servers.should_not include(@new_server_id)
+    end
+
+    it "should not exist" do
+      server = get_server(@hp_svc, @new_server_id)
+      server.id.should_not eql(@new_server_id)
     end
 
   end
