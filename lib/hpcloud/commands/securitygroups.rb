8c07461d1cfbd044b46f1184ac95ebac27cf2e11
Rupak Ganguly <rupakg@gmail.com>
2011-08-19 09:56:18 -0400

    Add securitygroups command and specs for it.

---
+++ lib/hpcloud/commands/securitygroups.rb
@@ -0,0 +1,31 @@
+module HP
+  module Cloud
+    class CLI < Thor
+
+      map 'securitygroups:list' => 'securitygroups'
+
+      desc "securitygroups", "list of available security groups"
+      long_desc <<-DESC
+  List the security groups in your compute account.
+
+Examples:
+  hpcloud securitygroups
+
+Aliases: securitygroups:list
+      DESC
+      def securitygroups
+        begin
+          securitygroups = connection(:compute).security_groups
+          if securitygroups.empty?
+            display "You currently have no security groups."
+          else
+            securitygroups.table([:name, :description, :owner_id])
+          end
+        rescue Excon::Errors::Forbidden => error
+          display_error_message(error)
+        end
+      end
+
+    end
+  end
+end
