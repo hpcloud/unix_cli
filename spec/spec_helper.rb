d4cf1b2b2c69aceeee462cdf36854657d71ad3e0
Rupak Ganguly <rupakg@gmail.com>
2011-09-08 15:51:11 -0400

    Cleanup internal urls and replace them with placeholders.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,27 +24,22 @@ require 'helpers/containers'
 
 RSpec.configure do |config|
   
-  # export KVS_TEST_HOST=16.49.184.31
-  # build/opt-centos5-x86_64/bin/stout-mgr create-account -port 9233 "Unix CLI" "unix.cli@hp.com"
-  #
-  # http://16.49.184.32:9242/kvs/keygen.html
   MOCKING_ENABLED = ENV['ENABLE_CLI_MOCKING'] || false
 
-  OS_STORAGE_AUTH_URL         = ENV['OS_STORAGE_AUTH_URL']  || "http://swift.nv1.devex.me:8080/auth/v1.0"  # "http://agpa-ge1.csbu.hpl.hp.com/auth/v1.0"
-  OS_STORAGE_ACCOUNT_USERNAME = ENV['OS_STORAGE_ACCOUNT_USERNAME']  || "dev_26879407200539:dev_77550147347364" # fog_tester1@test.com/FogTest123
-  OS_STORAGE_ACCOUNT_PASSWORD = ENV['OS_STORAGE_ACCOUNT_PASSWORD']  || "CuLqhwVWLaxXkFDYjPFUGNYAgsq4qu7c4U5"
-  OS_STORAGE_SEC_ACCOUNT_USERNAME = ENV['OS_STORAGE_SEC_ACCOUNT_USERNAME']  || "dev_31963196353900:dev_40709621046377"  # fog_tester2@test.com/FogTest123
-  OS_STORAGE_SEC_ACCOUNT_PASSWORD = ENV['OS_STORAGE_SEC_ACCOUNT_PASSWORD']  || "rhAz1B5ALIjzV1kuaduIwxY4T5t0awrFA7o"
+  ### set these env. vars with appropriate data manually.
+  OS_STORAGE_AUTH_URL         = ENV['OS_STORAGE_AUTH_URL'] || "https://objects.hpcloudsvc.com/auth/v1.0"
+  OS_STORAGE_ACCOUNT_USERNAME = ENV['OS_STORAGE_ACCOUNT_USERNAME'] || "your <access key 1>"
+  OS_STORAGE_ACCOUNT_PASSWORD = ENV['OS_STORAGE_ACCOUNT_PASSWORD'] || "<your secret key 1>"
+  OS_STORAGE_SEC_ACCOUNT_USERNAME = ENV['OS_STORAGE_SEC_ACCOUNT_USERNAME'] || "<your access key 2>"
+  OS_STORAGE_SEC_ACCOUNT_PASSWORD = ENV['OS_STORAGE_SEC_ACCOUNT_PASSWORD'] || "<your secret key 2>"
 
   if MOCKING_ENABLED
     puts "==========================================================="
     puts "Running tests in mocking mode..."
-    puts "BEWARE: It is an experimental attempt. Many tests may fail."
     puts "==========================================================="
     # Enable mocking
     Fog.mock!
   else
-#    puts "Running tests against KVS http://#{KVS_HOST}:#{KVS_PORT}..."
     puts "Running tests against HP OpenStack Storage (Swift) instance: #{OS_STORAGE_AUTH_URL}..."
   end
 end
