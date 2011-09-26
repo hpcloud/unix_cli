98c28b4d50166ca3f634f33a5c6aa2d8963dbf10
Rupak Ganguly <rupakg@gmail.com>
2011-09-26 18:12:15 -0400

    Modify account:setup command to invoke subcommands for storage and compute credentials.

--- lib/hpcloud/commands/account/setup.rb
+++ lib/hpcloud/commands/account/setup.rb
@@ -5,35 +5,21 @@ module HP
       desc 'account:setup', "set up or modify your credentials"
       long_desc <<-DESC
   Setup or modify your account credentials. This is generally the first step
-  in the process of using the Scalene command-line interface. 
+  in the process of using the HP Cloud command-line interface.
   
-  You will need your Username and Password from the HP Scalene web site to
+  You will need your Account ID and Account Key from the HP Cloud web site to
   set up your account. Optionally you can specify your own endpoint to access, 
   but in most cases you will want to use the default.  
   
-  You can re-run this command to modify your settings.
+  You can re-run this command to modify your settings or run account setup for
+  storage and compute separately as well.
       DESC
-      method_option 'no-validate', :type => :boolean, :default => false, 
+      method_option 'no-validate', :type => :boolean, :default => false,
                     :desc => "Don't verify account settings during setup"
       define_method "account:setup" do
-        credentials = {}
-        credentials[:account_id] = ask 'Account ID:'
-        credentials[:secret_key] = ask 'Account Key:'
-        credentials[:auth_uri] = ask_with_default 'API Auth Uri:',
-                                      Config.settings[:default_auth_uri]
-        unless options['no-validate']
-          begin
-            display "Verifying your account..."
-            connection_with(credentials).get_containers
-          rescue Excon::Errors::Forbidden => e
-            display_error_message(e)
-          # remove once this is handled more globally
-          rescue Excon::Errors::SocketError => e
-            display_error_message(e)
-          end
-        end
-        Config.update_credentials :default, credentials
-        display "Account credentials have been set up."
+        invoke "account:setup:storage"
+        puts ""
+        invoke "account:setup:compute"
       end
     
     end
