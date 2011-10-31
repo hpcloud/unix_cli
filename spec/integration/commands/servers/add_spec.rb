60a30223fe30eaf388fd77fe669d43c85bbe7692
Rupak Ganguly <rupakg@gmail.com>
2011-10-31 19:47:15 -0400

    Use constants rather than hardcoded values.

--- spec/integration/commands/servers/add_spec.rb
+++ spec/integration/commands/servers/add_spec.rb
@@ -12,7 +12,7 @@ describe "servers:add command" do
   ### Till we can get back the server id that is created, it cannot be deleted, hence marked pending
   context "when creating server with name, image and defaults" do
     before(:all) do
-      @response, @exit = run_command('servers:add fog-test-server ami-00000007').stdout_and_exit_status
+      @response, @exit = run_command("servers:add fog-test-server #{OS_COMPUTE_BASE_IMAGE_ID}").stdout_and_exit_status
       @new_server_id = @response.scan(/'([^']+)/)[0][0]
     end
 
@@ -21,9 +21,13 @@ describe "servers:add command" do
     end
     its_exit_status_should_be(:success)
 
-    it "should list in servers" do
+    it "should list id in servers" do
       servers = @hp_svc.servers.map {|s| s.id}
-      servers.should include(@new_server_id)
+      servers.should include(@new_server_id.to_i)
+    end
+    it "should list name in servers" do
+      servers = @hp_svc.servers.map {|s| s.name}
+      servers.should include("fog-test-server")
     end
 
     after(:all) do
