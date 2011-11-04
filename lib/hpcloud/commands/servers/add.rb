b87430879e0c720d56547ccd82fd25766e6814c3
Rupak Ganguly <rupakg@gmail.com>
2011-11-04 19:44:47 -0400

    Update help text, rename params and add name to table.

--- lib/hpcloud/commands/servers/add.rb
+++ lib/hpcloud/commands/servers/add.rb
@@ -2,24 +2,24 @@ module HP
   module Cloud
     class CLI < Thor
 
-      desc "servers:add <name> <image> <flavor>", "add a server"
+      desc "servers:add <name> <image_id> <flavor_id>", "add a server"
       long_desc <<-DESC
   Add a new server to your compute account. Server name can be specified with
   or without the preceding colon: 'my_server' or ':my_server'.
 
 Examples:
-  hpcloud servers:add :my_server ami-00000005              # Creates a new server named 'my_server' using supplied options \n
-  hpcloud servers:add :my_server ami-00000005 m1.tiny      # Creates a new server named 'my_server' using supplied options \n
+  hpcloud servers:add :my_server 7       # Creates a new server named 'my_server' using supplied options \n
+  hpcloud servers:add :my_server 7 1     # Creates a new server named 'my_server' using supplied options \n
 
 Aliases: none
       DESC
-      define_method "servers:add" do |name, image, flavor="1"|
+      define_method "servers:add" do |name, image_id, flavor_id="1"|
         # setup connection for compute service
         compute_connection = connection(:compute)
         begin
           # name cannot be assigned yet, only ids available
-          server = compute_connection.servers.new(:flavor_id => flavor,
-                                                  :image_id => image,
+          server = compute_connection.servers.new(:flavor_id => flavor_id,
+                                                  :image_id => image_id,
                                                   :name => name)
           server.save
           display "Created server #{name} with id '#{server.id}'."
@@ -29,7 +29,7 @@ Aliases: none
         # display list of servers
         servers = compute_connection.servers
         if !servers.empty?
-          servers.table([:id, :availability_zone, :groups, :flavor_id, :image_id, :created_at, :private_ip_address, :public_ip_address, :state])
+          servers.table([:id, :name, :flavor_id, :image_id, :created_at, :private_ip_address, :public_ip_address, :state])
         end
       end
 
