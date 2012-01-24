d67aa9fc67e2869b1a7b4cfae607c637ac3bd112
Rupak Ganguly <rupakg@gmail.com>
2012-01-24 13:35:34 -0500

    Update account setup to only accept one set of credentials instead of per services. This is ppossible due to CS integration.

--- lib/hpcloud/commands/account/setup.rb
+++ lib/hpcloud/commands/account/setup.rb
@@ -11,15 +11,40 @@ module HP
   set up your account. Optionally you can specify your own endpoint to access, 
   but in most cases you will want to use the default.  
   
-  You can re-run this command to modify your settings or run account setup for
-  storage and compute separately as well.
+  You can re-run this command to modify your settings.
       DESC
       method_option 'no-validate', :type => :boolean, :default => false,
                     :desc => "Don't verify account settings during setup"
       define_method "account:setup" do
-        invoke "account:setup:storage"
-        puts ""
-        invoke "account:setup:compute"
+        credentials = {}
+        display "****** Setup your HP Cloud Services account ******"
+        credentials[:account_id] = ask 'Account ID:'
+        credentials[:secret_key] = ask 'Account Key:'
+        credentials[:auth_uri] = ask_with_default 'Auth Uri:',
+                                      Config.settings[:default_auth_uri]
+        credentials[:tenant_id] = ask 'Tenant Id:'
+        unless options['no-validate']
+          begin
+            display "Verifying your HP Cloud Services account..."
+            begin
+              connection_with(:storage, credentials).head_containers
+            rescue NoMethodError
+              error "Your HP Cloud Services account does not have the Storage service activated."
+            end
+            begin
+              connection_with(:compute, credentials).list_servers
+            rescue NoMethodError
+              error "Your HP Cloud Services account does not have the Compute service activated."
+            end
+          rescue Excon::Errors::Forbidden, Excon::Errors::Unauthorized => e
+            display_error_message(e)
+          # remove once this is handled more globally
+          rescue Excon::Errors::SocketError => e
+            display_error_message(e)
+          end
+        end
+        Config.update_credentials :default, credentials
+        display "Account credentials for HP Cloud Services have been set up."
       end
     
     end
