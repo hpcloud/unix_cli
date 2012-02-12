70797489297208f67881f1a6559eea6cc3a4a3cc
Rupak Ganguly <rupakg@gmail.com>
2012-02-12 14:10:33 -0500

    Add capability to specify key name and security group while creating a server, with corresponding specs.

--- lib/hpcloud/commands/servers/add.rb
+++ lib/hpcloud/commands/servers/add.rb
@@ -2,26 +2,37 @@ module HP
   module Cloud
     class CLI < Thor
 
-      desc "servers:add <name> <image_id> <flavor_id>", "add a server"
+      desc "servers:add <name> <image_id> <flavor_id> <key_name> <sg_name>", "add a server"
       long_desc <<-DESC
-  Add a new server to your compute account. Server name can be specified with
-  or without the preceding colon: 'my_server' or ':my_server'.
+  Add a new server to your compute account. Optionally, A key name or a security group can be specified.
+  Server name can be specified with or without the preceding colon: 'my_server' or ':my_server'.
 
 Examples:
-  hpcloud servers:add :my_server 7 1     # Creates a new server named 'my_server' using supplied options \n
+  hpcloud servers:add :my_server 7 1            # Creates a new server named 'my_server' using supplied options \n
+  hpcloud servers:add :my_server 7 1 key1       # Creates a new server named 'my_server' using supplied options \n
+  hpcloud servers:add :my_server 7 1 key1 sg1   # Creates a new server named 'my_server' using supplied options \n
 
 Aliases: none
       DESC
-      define_method "servers:add" do |name, image_id, flavor_id|
+      define_method "servers:add" do |name, image_id, flavor_id, key_name=nil, sg_name=nil|
         # setup connection for compute service
         compute_connection = connection(:compute)
         begin
-          # name cannot be assigned yet, only ids available
-          server = compute_connection.servers.new(:flavor_id => flavor_id,
-                                                  :image_id => image_id,
-                                                  :name => name)
-          server.save
-          display "Created server '#{name}' with id '#{server.id}'."
+          if (key_name and sg_name)
+            server = compute_connection.servers.create(:flavor_id => flavor_id,
+                                                       :image_id => image_id,
+                                                       :name => name,
+                                                       :key_name => key_name,
+                                                       :security_groups => ["#{sg_name}"])
+            display "Created server '#{name}' with id '#{server.id}', key '#{key_name}' and security group '#{sg_name}'."
+          elsif (!key_name and !sg_name)
+            server = compute_connection.servers.create(:flavor_id => flavor_id,
+                                                       :image_id => image_id,
+                                                       :name => name)
+            display "Created server '#{name}' with id '#{server.id}'."
+          else
+            error "You need to specify both a key name and a security group.", :incorrect_usage
+          end
         rescue Excon::Errors::Unauthorized, Excon::Errors::Forbidden, Excon::Errors::Conflict => error
           display_error_message(error, :permission_denied)
         end
--- spec/integration/commands/servers/add_spec.rb
+++ spec/integration/commands/servers/add_spec.rb
@@ -7,12 +7,15 @@ describe "servers:add command" do
 
   before(:all) do
     @hp_svc = compute_connection
-    @server_name = resource_name("add")
+    @keypair_name = ('fog-serv-key-100')
+    @sg_name = resource_name('fog-serv-sg-100')
+    @keypair = @hp_svc.key_pairs.create(:name => @keypair_name)
+    @sgroup = @hp_svc.security_groups.create(:name => @sg_name, :description => "#{@sg_name} desc")
   end
 
-  ### Till we can get back the server id that is created, it cannot be deleted, hence marked pending
-  context "when creating server with name, image and defaults" do
+  context "when creating server with name, image and flavor (no keyname or security group)" do
     before(:all) do
+      @server_name = resource_name("add")
       @response, @exit = run_command("servers:add #{@server_name} #{OS_COMPUTE_BASE_IMAGE_ID} #{OS_COMPUTE_BASE_FLAVOR_ID}").stdout_and_exit_status
       @new_server_id = @response.scan(/'([^']+)/)[2][0]
     end
@@ -32,8 +35,60 @@ describe "servers:add command" do
     end
 
     after(:all) do
-      server = @hp_svc.servers.get(@new_server_id)
-      server.destroy
+      @hp_svc.delete_server(@new_server_id)
     end
   end
+  context "when creating server with name, image, flavor, keyname and security group" do
+    before(:all) do
+      @server_name = resource_name("add")
+      @response, @exit = run_command("servers:add #{@server_name} #{OS_COMPUTE_BASE_IMAGE_ID} #{OS_COMPUTE_BASE_FLAVOR_ID} #{@keypair_name} #{@sg_name}").stdout_and_exit_status
+      @new_server_id = @response.scan(/'([^']+)/)[2][0]
+    end
+
+    it "should show success message" do
+      @response.should eql("Created server '#{@server_name}' with id '#{@new_server_id}', key '#{@keypair_name}' and security group '#{@sg_name}'.\n")
+    end
+    its_exit_status_should_be(:success)
+
+    it "should list id in servers" do
+      servers = @hp_svc.servers.map {|s| s.id}
+      servers.should include(@new_server_id.to_i)
+    end
+    it "should list name in servers" do
+      servers = @hp_svc.servers.map {|s| s.name}
+      servers.should include(@server_name)
+    end
+
+    after(:all) do
+      @hp_svc.delete_server(@new_server_id)
+    end
+  end
+  context "when creating server with name, image, flavor and only keyname" do
+    before(:all) do
+      @server_name = resource_name("add")
+      @response, @exit = run_command("servers:add #{@server_name} #{OS_COMPUTE_BASE_IMAGE_ID} #{OS_COMPUTE_BASE_FLAVOR_ID} #{@keypair_name}").stderr_and_exit_status
+    end
+
+    it "should show error message" do
+      @response.should eql("You need to specify both a key name and a security group.\n")
+    end
+    its_exit_status_should_be(:incorrect_usage)
+  end
+  context "when creating server with name, image, flavor and only security group" do
+    before(:all) do
+      @server_name = resource_name("add")
+      @response, @exit = run_command("servers:add #{@server_name} #{OS_COMPUTE_BASE_IMAGE_ID} #{OS_COMPUTE_BASE_FLAVOR_ID} #{@sg_name}").stderr_and_exit_status
+    end
+
+    it "should show error message" do
+      @response.should eql("You need to specify both a key name and a security group.\n")
+    end
+    its_exit_status_should_be(:incorrect_usage)
+  end
+
+  after(:all) do
+    @keypair.destroy if @keypair
+    @sgroup.destroy if @sgroup
+  end
+
 end
