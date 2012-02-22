cec27ece123bae1afdb4f9f7076eb1944fdf524b
Rupak Ganguly <rupakg@gmail.com>
2012-02-21 21:54:33 -0500

    Refactor keypairs:add to use options to specify fingerprint and private key data to create key pairs, and update corresponding specs.

--- lib/hpcloud/commands/keypairs/add.rb
+++ lib/hpcloud/commands/keypairs/add.rb
@@ -8,27 +8,36 @@ module HP
   Additionally you can use the -o option to save the key into a file.
 
 Examples:
-  hpcloud keypairs:add mykey
-  hpcloud keypairs:add mykey c1:db:b5:bc:8b:b9:0f:33:62:53:de:80:6e:ae:67:66
-  hpcloud keypairs:add mykey c1:db:b5:bc:8b:b9:0f:33:62:53:de:80:6e:ae:67:66 'private key data'
-  hpcloud keypairs:add mykey -o               # creates a file mykey.pem in the current folder
+  hpcloud keypairs:add mykey                                           # creates a key 'mykey'
+  hpcloud keypairs:add mykey -f <fingerprint>                          # creates a key 'mykey' using the supplied fingerprint
+  hpcloud keypairs:add mykey -p 'private key data'                     # creates a key 'mykey' using the supplied private key data
+  hpcloud keypairs:add mykey -f <fingerprint> -p 'private key data'    # creates a key 'mykey' using the supplied fingerprint and private key data
+  hpcloud keypairs:add mykey -o                                        # creates a key and saves it to mykey.pem file in the current folder
 
 Aliases: none
       DESC
+      method_option :fingerprint, :type => :string, :aliases => '-f', :desc => 'Specify a fingerprint to be used.'
+      method_option :private_key, :type => :string, :aliases => '-p', :desc => 'Specify private key data to be used.'
       method_option :output, :default => false, :type => :boolean, :aliases => '-o', :desc => 'Save the key pair to a file in the current folder.'
-      define_method "keypairs:add" do |key_name, fingerprint=nil, private_key=nil|
+      define_method "keypairs:add" do |key_name|
         begin
+          # get the options
+          fingerprint = options[:fingerprint]
+          private_key = options[:private_key]
+          # connect
           compute_connection = connection(:compute)
           keypair = compute_connection.key_pairs.create(:name => key_name, :fingerprint => fingerprint, :private_key => private_key)
           if options.output?
             keypair.write("./#{keypair.name}.pem")
-            display "Created key pair '#{key_name}' and saved it in a file at './#{keypair.name}.pem'."
+            display "Created key pair '#{key_name}' and saved it to a file at './#{keypair.name}.pem'."
           else
             display keypair.private_key
             display "Created key pair '#{key_name}'."
           end
         rescue Fog::Compute::HP::Error
-          display "Key pair '#{key_name}' already exists."
+          error "Key pair '#{key_name}' already exists.", :general_error
+        rescue Excon::Errors::BadRequest => error
+          display_error_message(error, :general_error)
         rescue Excon::Errors::Unauthorized, Excon::Errors::Forbidden => error
           display_error_message(error, :permission_denied)
         end
--- spec/integration/commands/keypairs/add_spec.rb
+++ spec/integration/commands/keypairs/add_spec.rb
@@ -7,72 +7,160 @@ describe "keypairs:add command" do
 
   before(:all) do
     @hp_svc = compute_connection
+    @fingerprint = "c1:db:b5:bc:8b:b9:0f:33:62:53:de:80:6e:ae:67:66"
+    @private_data = "some real private data"
   end
 
   context "when creating keypair with name" do
     before(:all) do
-      @response, @exit = run_command('keypairs:add mykey').stdout_and_exit_status
+      @key_name = 'fog-key-200'
+      @response, @exit = run_command("keypairs:add #{@key_name}").stdout_and_exit_status
     end
 
     it "should show success message" do
-      @response.should include("Created key pair 'mykey'.")
+      @response.should include("Created key pair '#{@key_name}'.")
     end
     its_exit_status_should_be(:success)
 
     it "should list in keypairs" do
       keypairs = @hp_svc.key_pairs.map {|kp| kp.name}
-      keypairs.should include('mykey')
+      keypairs.should include(@key_name)
     end
 
     it "should have a name" do
-      keypair = get_keypair(@hp_svc, 'mykey')
-      keypair.name.should eql('mykey')
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.name.should eql(@key_name)
     end
 
     it "should have a fingerprint data" do
-      keypair = get_keypair(@hp_svc, 'mykey')
+      keypair = get_keypair(@hp_svc, @key_name)
       keypair.fingerprint.should_not be_nil
     end
 
     pending "should report key exists if created again - Bug NOV-1479" do
-      @response, @exit = run_command('keypairs:add mykey').stdout_and_exit_status
-      @response.should eql("Key pair 'mykey' already exists.\n")
+      @response, @exit = run_command("keypairs:add #{@key_name}").stderr_and_exit_status
+      @response.should eql("Key pair '#{@key_name}' already exists.\n")
     end
 
     after(:all) do
-      keypair = get_keypair(@hp_svc, 'mykey')
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.destroy if keypair
+    end
+  end
+
+  context "when creating keypair with name and fingerprint" do
+    before(:all) do
+      @key_name = 'fog-key-201'
+      @response, @exit = run_command("keypairs:add #{@key_name} -f #{@fingerprint}").stdout_and_exit_status
+    end
+    its_exit_status_should_be(:success)
+
+    it "should list in keypairs" do
+      keypairs = @hp_svc.key_pairs.map {|kp| kp.name}
+      keypairs.should include(@key_name)
+    end
+
+    it "should have a name" do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.name.should eql(@key_name)
+    end
+
+    it "should have a fingerprint data" do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.fingerprint.should_not be_nil
+    end
+
+    after(:all) do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.destroy if keypair
+    end
+  end
+
+  context "when creating keypair with name and private data" do
+    before(:all) do
+      @key_name = 'fog-key-203'
+      @response, @exit = run_command("keypairs:add #{@key_name} -p #{@private_data}").stdout_and_exit_status
+    end
+    its_exit_status_should_be(:success)
+
+    it "should list in keypairs" do
+      keypairs = @hp_svc.key_pairs.map {|kp| kp.name}
+      keypairs.should include(@key_name)
+    end
+
+    it "should have a name" do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.name.should eql(@key_name)
+    end
+
+    it "should have a fingerprint data" do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.fingerprint.should_not be_nil
+    end
+
+    after(:all) do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.destroy if keypair
+    end
+  end
+
+  context "when creating keypair with name, fingerprint and private data" do
+    before(:all) do
+      @key_name = 'fog-key-204'
+      @response, @exit = run_command("keypairs:add #{@key_name} -f #{@fingerprint} -p #{@private_data}").stdout_and_exit_status
+    end
+    its_exit_status_should_be(:success)
+
+    it "should list in keypairs" do
+      keypairs = @hp_svc.key_pairs.map {|kp| kp.name}
+      keypairs.should include(@key_name)
+    end
+
+    it "should have a name" do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.name.should eql(@key_name)
+    end
+
+    it "should have a fingerprint data" do
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.fingerprint.should_not be_nil
+    end
+
+    after(:all) do
+      keypair = get_keypair(@hp_svc, @key_name)
       keypair.destroy if keypair
     end
   end
 
   context "when creating keypair with output flag" do
     before(:all) do
-      @response, @exit = run_command('keypairs:add mykey2 -o').stdout_and_exit_status
+      @key_name = 'fog-key-205'
+      @response, @exit = run_command("keypairs:add #{@key_name} -o").stdout_and_exit_status
     end
     it "should show success message" do
-      @response.should include("Created key pair 'mykey2' and saved it in a file at './mykey2.pem'.")
+      @response.should include("Created key pair '#{@key_name}' and saved it to a file at './#{@key_name}.pem'.")
     end
     its_exit_status_should_be(:success)
 
     it "should list in keypairs" do
       keypairs = @hp_svc.key_pairs.map {|kp| kp.name}
-      keypairs.should include('mykey2')
+      keypairs.should include(@key_name)
     end
 
     it "should have a name" do
-      keypair = get_keypair(@hp_svc, 'mykey2')
-      keypair.name.should eql('mykey2')
+      keypair = get_keypair(@hp_svc, @key_name)
+      keypair.name.should eql(@key_name)
     end
 
     it "should have a fingerprint data" do
-      keypair = get_keypair(@hp_svc, 'mykey2')
+      keypair = get_keypair(@hp_svc, @key_name)
       keypair.fingerprint.should_not be_nil
     end
 
     after(:all) do
-      keypair = get_keypair(@hp_svc, 'mykey2')
+      keypair = get_keypair(@hp_svc, @key_name)
       keypair.destroy if keypair
-      File.delete("./mykey2.pem") if File.exists?("./mykey2.pem")
+      File.delete("./#{@key_name}.pem") if File.exists?("./#{@key_name}.pem")
     end
   end
 end
