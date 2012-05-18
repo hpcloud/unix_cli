8b71e78c3270b337fac6f8204a871b03cd972109
Rupak Ganguly <rupakg@gmail.com>
2012-05-18 17:12:57 -0400

    Add options param to be passed into connection methods. Add a new validation account method to validate credentials with the identity service. Update exception handling in connection methods.

--- lib/hpcloud/cli.rb
+++ lib/hpcloud/cli.rb
@@ -15,79 +15,101 @@ module HP
                     }
     
       private
-      def connection(service = :storage)
+      def connection(service = :storage, options = {})
+        begin
         if service == :storage
-          storage_connection
+          storage_connection(options)
         elsif service == :compute
-          compute_connection
+          compute_connection(options)
         elsif service == :cdn
-          cdn_connection
+          cdn_connection(options)
+        end
+        rescue Exception => e
+          error "Please check your HP Cloud Services account to make sure the '#{service.to_s.capitalize!}' service is activated for the appropriate availability zone.\n Exception: #{e}"
         end
       end
 
-      def storage_connection
+      def storage_connection(options = {})
         return @storage_connection if @storage_connection
         storage_credentials = Config.current_credentials
         if storage_credentials
-          @storage_connection ||= connection_with(:storage, storage_credentials)
+          @storage_connection ||= connection_with(:storage, storage_credentials, options)
         else
-          error "Please check your HP Cloud Services account to make sure the Storage service is activated."
+          error "Error in connecting to the Storage service. Please check your HP Cloud Services account to make sure the account credentials are correct."
         end
       end
 
-      def compute_connection
+      def compute_connection(options = {})
         return @compute_connection if @compute_connection
         compute_credentials = Config.current_credentials
         if compute_credentials
-          @compute_connection ||= connection_with(:compute, compute_credentials)
+          @compute_connection ||= connection_with(:compute, compute_credentials, options)
         else
-          error "Please check your HP Cloud Services account to make sure the Compute service is activated."
+          error "Error in connecting to the Compute service. Please check your HP Cloud Services account to make sure the account credentials are correct."
         end
       end
 
-      def cdn_connection
+      def cdn_connection(options = {})
         return @cdn_connection if @cdn_connection
         cdn_credentials = Config.current_credentials
         if cdn_credentials
-          @cdn_connection ||= connection_with(:cdn, cdn_credentials)
+          @cdn_connection ||= connection_with(:cdn, cdn_credentials, options)
         else
-          error "Please check your HP Cloud Services account to make sure the CDN service is activated."
+          error "Error in connecting to the CDN service. Please check your HP Cloud Services account to make sure the account credentials are correct."
         end
       end
 
-      def connection_with(service, service_credentials)
-        connection_options = {:connect_timeout => Config::CONNECT_TIMEOUT || 5,
-                            :read_timeout    => Config::READ_TIMEOUT || 5,
-                            :write_timeout   => Config::WRITE_TIMEOUT || 5}
-        begin
-          if service == :storage
-            Fog::Storage.new( :provider        => 'HP',
-                              :connection_options => connection_options,
-                              :hp_account_id   => service_credentials[:account_id],
-                              :hp_secret_key   => service_credentials[:secret_key],
-                              :hp_auth_uri     => service_credentials[:auth_uri],
-                              :hp_tenant_id    => service_credentials[:tenant_id])
-          elsif service == :compute
-            Fog::Compute.new( :provider        => 'HP',
-                              :connection_options => connection_options,
-                              :hp_account_id   => service_credentials[:account_id],
-                              :hp_secret_key   => service_credentials[:secret_key],
-                              :hp_auth_uri     => service_credentials[:auth_uri],
-                              :hp_tenant_id    => service_credentials[:tenant_id],
-                              :hp_avl_zone     => Config.settings[:availability_zone].to_sym)
-          elsif service == :cdn
-            Fog::CDN.new( :provider        => 'HP',
-                              :connection_options => connection_options,
-                              :hp_account_id   => service_credentials[:account_id],
-                              :hp_secret_key   => service_credentials[:secret_key],
-                              :hp_auth_uri     => service_credentials[:auth_uri],
-                              :hp_tenant_id    => service_credentials[:tenant_id])
-          end
-        rescue
-          error "Error connecting to the service endpoint at: #{service_credentials[:auth_uri]}."
+      def connection_with(service, service_credentials, options={})
+        connection_options = {:connect_timeout => Config.settings[:connect_timeout] || Config::CONNECT_TIMEOUT,
+                              :read_timeout    => Config.settings[:read_timeout]    || Config::READ_TIMEOUT,
+                              :write_timeout   => Config.settings[:write_timeout]   || Config::WRITE_TIMEOUT,
+                              :ssl_verify_peer => Config.settings[:ssl_verify]      || false,
+                              :ssl_ca_path     => Config.settings[:ssl_ca_path]     || nil,
+                              :ssl_ca_file     => Config.settings[:ssl_ca_file]     || nil}
+        if service == :storage
+          Fog::Storage.new( :provider        => 'HP',
+                            :connection_options => connection_options,
+                            :hp_account_id   => service_credentials[:account_id],
+                            :hp_secret_key   => service_credentials[:secret_key],
+                            :hp_auth_uri     => service_credentials[:auth_uri],
+                            :hp_tenant_id    => service_credentials[:tenant_id],
+                            :hp_avl_zone     => options[:avl_zone] || Config.settings[:storage_avl_zone])
+        elsif service == :compute
+          Fog::Compute.new( :provider        => 'HP',
+                            :connection_options => connection_options,
+                            :hp_account_id   => service_credentials[:account_id],
+                            :hp_secret_key   => service_credentials[:secret_key],
+                            :hp_auth_uri     => service_credentials[:auth_uri],
+                            :hp_tenant_id    => service_credentials[:tenant_id],
+                            :hp_avl_zone     => options[:avl_zone] || Config.settings[:compute_avl_zone])
+        elsif service == :cdn
+          Fog::CDN.new( :provider            => 'HP',
+                            :connection_options => connection_options,
+                            :hp_account_id   => service_credentials[:account_id],
+                            :hp_secret_key   => service_credentials[:secret_key],
+                            :hp_auth_uri     => service_credentials[:auth_uri],
+                            :hp_tenant_id    => service_credentials[:tenant_id],
+                            :hp_avl_zone     => options[:avl_zone] || Config.settings[:cdn_avl_zone])
         end
       end
-    
+
+      def validate_account(account_credentials)
+        connection_options = {:connect_timeout => Config.settings[:connect_timeout] || Config::CONNECT_TIMEOUT,
+                              :read_timeout    => Config.settings[:read_timeout]    || Config::READ_TIMEOUT,
+                              :write_timeout   => Config.settings[:write_timeout]   || Config::WRITE_TIMEOUT,
+                              :ssl_verify_peer => Config.settings[:ssl_verify]      || false,
+                              :ssl_ca_path     => Config.settings[:ssl_ca_path]     || nil,
+                              :ssl_ca_file     => Config.settings[:ssl_ca_file]     || nil}
+        options = {
+            :hp_account_id   => account_credentials[:account_id],
+            :hp_secret_key   => account_credentials[:secret_key],
+            :hp_auth_uri     => account_credentials[:auth_uri],
+            :hp_tenant_id    => account_credentials[:tenant_id]
+        }
+        # authenticate with Identity service
+        Fog::HP.authenticate_v2(options, connection_options)
+      end
+
       # print some non-error output to the user
       def display(message)
         say message unless @silence_display
