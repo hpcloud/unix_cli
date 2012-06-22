df3134c9b799c5850dd39c2cf3650bf8fefb474b
Rupak Ganguly <rupakg@gmail.com>
2012-06-22 17:11:30 -0400

    Fix connections to allow ssl_verify_peer as false.

--- spec/helpers/connections.rb
+++ spec/helpers/connections.rb
@@ -64,13 +64,13 @@ module HP::Cloud
   
     private
 
-    # override #connection not to look at account files, just use hardcoded
-    # test credentials.
-    def connection(service = :storage, options = {})
+    #override #connection not to look at account files, just use hardcoded
+    #test credentials.
+    def connection(service = :storage, options={})
       begin
         if service == :storage
           Fog::Storage.new( :provider        => 'HP',
-                            :connection_options => default_connection_options(:ssl_verify_peer => false),
+                            :connection_options => default_connection_options,
                             :hp_account_id   => OS_STORAGE_ACCOUNT_USERNAME,
                             :hp_secret_key   => OS_STORAGE_ACCOUNT_PASSWORD,
                             :hp_auth_uri     => OS_STORAGE_AUTH_URL,
@@ -78,7 +78,7 @@ module HP::Cloud
                             :hp_avl_zone     => options[:availability_zone] || OS_STORAGE_ACCOUNT_AVL_ZONE)
         elsif service == :compute
           Fog::Compute.new( :provider        => 'HP',
-                            :connection_options => default_connection_options(:ssl_verify_peer => false),
+                            :connection_options => default_connection_options,
                             :hp_account_id   => OS_COMPUTE_ACCOUNT_USERNAME,
                             :hp_secret_key   => OS_COMPUTE_ACCOUNT_PASSWORD,
                             :hp_auth_uri     => OS_COMPUTE_AUTH_URL,
@@ -86,7 +86,7 @@ module HP::Cloud
                             :hp_avl_zone     => options[:availability_zone] || OS_COMPUTE_ACCOUNT_AVL_ZONE)
         elsif service == :cdn
           Fog::CDN.new( :provider        => 'HP',
-                            :connection_options => default_connection_options(:ssl_verify_peer => false),
+                            :connection_options => default_connection_options,
                             :hp_account_id   => OS_STORAGE_ACCOUNT_USERNAME,
                             :hp_secret_key   => OS_STORAGE_ACCOUNT_PASSWORD,
                             :hp_auth_uri     => OS_STORAGE_AUTH_URL,
@@ -98,6 +98,17 @@ module HP::Cloud
       end
 
     end
-  
+
+    def default_connection_options(options={})
+      # define default connection options
+      {
+          :connect_timeout => options[:connect_timeout] || Config::CONNECT_TIMEOUT,
+          :read_timeout    => options[:read_timeout] || Config::READ_TIMEOUT,
+          :write_timeout   => options[:write_timeout] || Config::WRITE_TIMEOUT,
+          :ssl_verify_peer => options[:ssl_verify_peer] || false,
+          :ssl_ca_path     => options[:ssl_ca_path],
+          :ssl_ca_file     => options[:ssl_ca_file]
+      }
+    end
   end
 end
