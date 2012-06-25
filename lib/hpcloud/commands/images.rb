0c279ee0edfeee5b858bc2a24d8a50d407840d18
Rupak Ganguly <rupakg@gmail.com>
2012-06-25 16:36:40 -0400

    Remove missing data from images list.

--- lib/hpcloud/commands/images.rb
+++ lib/hpcloud/commands/images.rb
@@ -26,7 +26,7 @@ Aliases: images:list
           if images.empty?
             display "You currently have no images to use."
           else
-            images.table([:id, :name, :minDisk, :minRam, :created_at, :updated_at, :status])
+            images.table([:id, :name, :created_at, :status])
           end
         rescue Fog::HP::Errors::ServiceError, Fog::Compute::HP::Error => error
           display_error_message(error, :general_error)
