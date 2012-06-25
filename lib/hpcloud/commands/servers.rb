7056f72dab2cc51e343b7bf0a2edbf1eda92e78e
Rupak Ganguly <rupakg@gmail.com>
2012-06-25 16:40:46 -0400

    Add new attributes to the servers list command.

--- lib/hpcloud/commands/servers.rb
+++ lib/hpcloud/commands/servers.rb
@@ -28,14 +28,33 @@ Aliases: servers:list
           if servers.empty?
             display "You currently have no servers, use `#{selfname} servers:add <name>` to create one."
           else
-            #servers.table([:id, :name, :image_id, :flavor_id, :created_at, :key_name, :public_ip, :private_ip, :state])
-            servers.table([:id, :name, :created_at, :key_name, :state])
+            tablelize(get_server_data(servers), [:id, :name, :flavor, :image, :public_ip, :private_ip, :keyname, :security_groups, :created, :state])
           end
         rescue Fog::HP::Errors::ServiceError, Fog::Compute::HP::Error => error
           display_error_message(error, :general_error)
         rescue Excon::Errors::Unauthorized, Excon::Errors::Forbidden => error
           display_error_message(error, :permission_denied)
         end
+      end
+
+      private
+
+      def get_server_data(servers)
+        s_attr = []
+        servers.map { |s| s_attr << { :id => s.id,
+                                      :name => s.name,
+                                      :flavor => s.flavor_id,
+                                      :image => s.image_id,
+                                      :public_ip => s.public_ip_address,
+                                      :private_ip => s.private_ip_address,
+                                      :keyname => s.key_name,
+                                      :security_groups => s.security_groups.map {|sg| sg["name"]}.join(', '),
+                                      :created => s.created_at,
+                                      :state => s.state
+                                      #:metadata => s.metadata.map {|m| "#{m.key} => #{m.value}"}.join(', ')
+                                    }
+                    }
+        s_attr
       end
 
     end
