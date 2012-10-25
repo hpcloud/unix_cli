e49cf7ce3b51b41c85fd3f6f8feb16bd035eb269
Douglas Moran <douglas.moran@hp.com>
2012-10-25 15:17:04 -0500

    Update lib/hpcloud/commands/cdn_containers/add.rb

--- lib/hpcloud/commands/cdn_containers/add.rb
+++ lib/hpcloud/commands/cdn_containers/add.rb
@@ -4,11 +4,11 @@ module HP
 
       desc "cdn:containers:add name [name ...]", "Add containers to the CDN."
       long_desc <<-DESC
-  Add existing containers from your storage account to the CDN. Container names can be specified with or without the preceding colon: 'my_container' or ':my_container'. Optionally, an availability zone can be passed.
+  Add existing containers from your storage account to the CDN. Container names can be specified with or without the preceding colon: 'my_container' or ':my_container'. Optionally, you can specify an availability zone.
 
 Examples:
-  hpcloud cdn:containers:add :tainer1 :tainer2                    # Adds the containers to the CDN
-  hpcloud cdn:containers:add :my_cdn_container -z region-a.geo-1  # Add the container to the CDN in the specified availability zone
+  hpcloud cdn:containers:add :tainer1 :tainer2                    # Add the containers`tainer1` and `tainer2` to the CDN:
+  hpcloud cdn:containers:add :my_cdn_container -z region-a.geo-1  # Add the container `my_cdn_container` to the CDN in the  availability zone `region-a.geo`:
       DESC
       CLI.add_common_options
       define_method "cdn:containers:add" do |name|
