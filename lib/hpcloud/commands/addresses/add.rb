d6b03105a7dc66c380849123d7a970992dedf509
Douglas Moran <douglas.moran@hp.com>
2012-10-25 13:25:36 -0500

    Update lib/hpcloud/commands/addresses/add.rb

--- lib/hpcloud/commands/addresses/add.rb
+++ lib/hpcloud/commands/addresses/add.rb
@@ -6,11 +6,11 @@ module HP
 
       desc "addresses:add", "Add or allocate a new public IP address."
       long_desc <<-DESC
-  Add or allocate a new public IP address from the pool of available IP addresses.  Optionally, an availability zone can be passed.
+  Add or allocate a new public IP address from the pool of available IP addresses.  Optionally, you can set an availability zone.
 
 Examples:
-  hpcloud addresses:add
-  hpcloud addresses:add -z az-2.region-a.geo-1
+  hpcloud addresses:add # Add a new public IP address:
+  hpcloud addresses:add -z az-2.region-a.geo-1 # Add a new public IP address in availability zone `az-2.region-a.geo-1`:
 
 Aliases: addresses:allocate
       DESC
