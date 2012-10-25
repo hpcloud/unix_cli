970f898ca93fe601cb3df2c7a16f13f37cb600a1
Douglas Moran <douglas.moran@hp.com>
2012-10-25 13:22:08 -0500

    Update lib/hpcloud/commands/account/use.rb

--- lib/hpcloud/commands/account/use.rb
+++ lib/hpcloud/commands/account/use.rb
@@ -9,7 +9,7 @@ module HP
   Use the specified account as your default account.  This command overwrites the default account.
   
 Examples:
-  hpcloud account:use useast # Sets the default account to `useast`
+  hpcloud account:use useast # Set the default account to `useast`:
       DESC
       define_method "account:use" do |name|
         cli_command(options) {
