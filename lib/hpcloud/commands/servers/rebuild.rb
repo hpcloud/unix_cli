bc2d24ea11e2ac881f0b6c526aa17d3316c5e87f
Douglas Moran <douglas.moran@hp.com>
2012-10-29 09:45:55 -0500

    Update lib/hpcloud/commands/servers/rebuild.rb

--- lib/hpcloud/commands/servers/rebuild.rb
+++ lib/hpcloud/commands/servers/rebuild.rb
@@ -4,12 +4,12 @@ module HP
 
       desc "servers:rebuild name_or_id [image_name_or_id]", "Rebuild a server (specified by server name or ID)."
       long_desc <<-DESC
-  Rebuild an existing server specified name or id. Optionally, the server may be rebuilt with a new image.  Rebuilding a server may take some time so it might be necessary to check the status of the server by issuing command, 'hpcloud servers'. Optionally, an availability zone can be passed.
+  Rebuild an existing server specified name or ID. Optionally, the server may be rebuilt with a new image.  Rebuilding a server may take some time so it might be necessary to check the status of the server by issuing the command 'hpcloud servers'. Optionally, you can specify an availability zone.
 
 Examples:
-  hpcloud servers:rebuild Hal9000    # rebuild 'Hal9000'
-  hpcloud servers:rebuild 1003 222   # rebuild server 1003 with image 222
-  hpcloud servers:rebuild DeepThought -z az-2.region-a.geo-1    # Optionally specify an availability zone
+  hpcloud servers:rebuild Hal9000    # Rebuild server 'Hal9000':
+  hpcloud servers:rebuild 1003 222   # Rebuild server 1003 with image 222:
+  hpcloud servers:rebuild DeepThought -z az-2.region-a.geo-1    # Rebuild server `DeepThought` for availability zone `az-2.region-a.geo-1`:
       DESC
       CLI.add_common_options
       define_method "servers:rebuild" do |name_or_id, image_name_or_id=nil|
