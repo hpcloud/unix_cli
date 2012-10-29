ffd16c69aaadf82bafc1606ea7ddd9f26a908f36
Douglas Moran <douglas.moran@hp.com>
2012-10-29 15:46:03 -0500

    Update lib/hpcloud/commands/acl.rb

--- lib/hpcloud/commands/acl.rb
+++ lib/hpcloud/commands/acl.rb
@@ -8,12 +8,12 @@ module HP
 
       desc 'acl <object/container>', "View the ACL for an object or container."
       long_desc <<-DESC
-  View the Access Control List (ACL) for a container or object. Optionally, an availability zone can be passed.
+  View the access control list (ACL) for a container or object. Optionally, you can specify an availability zone.
 
 Examples:
-  hpcloud acl :my_container/my_file.txt         # Get ACL for object 'my_file.txt'
-  hpcloud acl :my_container                     # Get ACL for container 'my_container'
-  hpcloud acl :my_container -z region-a.geo-1  # Get ACL for container 'my_container' by specifying an availability zone
+  hpcloud acl :my_container/my_file.txt         # Display the ACL for the object 'my_file.txt':
+  hpcloud acl :my_container                     # Display the ACL for the container 'my_container':
+  hpcloud acl :my_container -z region-a.geo-1  # Display the ACL for the container 'my_container' for availability zone `region-a.geo-1`:
       DESC
       CLI.add_common_options
       def acl(name, *names)
