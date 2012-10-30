dfc40e972e45c25b12757a23a3dbb0805f5aa419
Douglas Moran <douglas.moran@hp.com>
2012-10-30 09:20:15 -0500

    Update lib/hpcloud/commands/list.rb

--- lib/hpcloud/commands/list.rb
+++ lib/hpcloud/commands/list.rb
@@ -7,16 +7,14 @@ module HP
     
       desc 'list <container>', "List container contents."
       long_desc <<-DESC
-  List the contents of a specified container. Optionally, an availability zone can be passed.
-
-Listing details on files will be available in a future release.
+  List the contents of a specified container. Optionally, you can specify an availability zone.
 
 
 Examples:
-  hpcloud list :tainer/1.txt :tainer/2.txt      # List the two objects
-  hpcloud list :tainer                          # List the all the objects in the container
-  hpcloud list                                  # List all containers
-  hpcloud list :my_container -z region-a.geo-1  # Optionally specify an availability zone
+  hpcloud list :tainer/1.txt :tainer/2.txt      # List the two objects `1.txt` and 2.txt` in the container `tainer`:
+  hpcloud list :tainer                          # List all the objects in container `tainer`:
+  hpcloud list                                  # List all containers:
+  hpcloud list :my_container -z region-a.geo-1  # List all the objects in container `my_container` for availability zone `region-a.geo-1`:
 
 Aliases: ls
       DESC
