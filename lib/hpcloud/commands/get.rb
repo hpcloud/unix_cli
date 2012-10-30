c99b30cfd0af7f7f210876e1c34b34aeb72d9dee
Douglas Moran <douglas.moran@hp.com>
2012-10-30 08:22:10 -0500

    Update lib/hpcloud/commands/get.rb

--- lib/hpcloud/commands/get.rb
+++ lib/hpcloud/commands/get.rb
@@ -6,11 +6,11 @@ module HP
 
       desc 'get object [object ...]', 'Fetch objects to your local directory.'
       long_desc <<-DESC
-  Copy remote objects from a container to your current directory. Optionally, an availability zone can be passed.
+  Copy remote objects from a container to your current directory. Optionally, you can specify an availability zone.
 
 Examples: 
-  hpcloud get :my_container/file.txt :my_container/resume.txt # copy file.txt and resume.txt to current directory
-  hpcloud get :my_container/file.txt -z region-a.geo-1   # Optionally specify an availability zone
+  hpcloud get :my_container/file.txt :my_container/resume.txt  # Copy `file.txt` and `resume.txt` to your current directory:
+  hpcloud get :my_container/file.txt -z region-a.geo-1    # Copy `file.txt` to your current directory for availability zone `region-a.geo-1`:
 
 Aliases: fetch
       DESC
