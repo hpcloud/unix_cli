42d78d755bb299a16c651008c766a364096b4e56
howete <thowe@hp.com>
2012-11-13 16:06:03 -0700

    dxcli-493 automate release notes generation

---
+++ notes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Build the release notes page
+#
+COLUMNS=256;
+LINES=24;
+export COLUMNS LINES;
+NOTES=notes.txt
+DATE=$(date +'%D')
+RELEASE=$(cat lib/hpcloud/version.rb)
+
+function toc { sed -e 's/^##Release \(.*\) Features##/##Release \1 Features## {\1}/' -e 's/Features## {\([^.]*\).\([^.]*\).\([^.]*\)}/Features## {#v\1_\2_\3}/' CHANGELOG; }
+
+echo "These are the release notes for the HP Cloud services UNIX CLI.  The current release number for the [UNIX CLI software](/cli/unix) is version ${VERSION}, released on ${DATE}." >${NOTES}
+echo >>${NOTES}
+toc | grep '##Release' | sed -e 's/^##/* [/' -e 's/## /]/' -e 's/{/(/' -e 's/\}/)/' >>${NOTES}
+toc >>${NOTES}
+
+CONTAINER="documentation-downloads"
+DEST=":${CONTAINER}/unixcli/"
+cat ${NOTES}
+hpcloud copy -a deploy ${NOTES} $DEST
+hpcloud location -a deploy ${DEST}${NOTES}
+rm -f ${NOTES}
