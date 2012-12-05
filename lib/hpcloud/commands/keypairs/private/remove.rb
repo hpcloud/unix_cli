4120659d7e4e623a656aa26a21a45973b31c9e5b
howete <thowe@hp.com>
2012-12-05 12:57:41 -0700

    dxcli-413 add some aliases

--- lib/hpcloud/commands/keypairs/private/remove.rb
+++ lib/hpcloud/commands/keypairs/private/remove.rb
@@ -2,12 +2,16 @@ module HP
   module Cloud
     class CLI < Thor
 
+      map %w(keypairs:private:rm keypairs:private:delete keypairs:private:destroy keypairs:private:del) => 'keypairs:private:remove'
+
       desc "keypairs:private:remove <key_name> [key_name...]", "Make a private key available for the CLI"
       long_desc <<-DESC
   This command copies the private key file to ~/.hpcloud/keypairs directory so the CLI can use it for various commands to access servers.  This command does *not* upload the private key anywhere and it will *only* be available for the CLI on the current server.
 
 Examples:
   hpcloud keypairs:private:remove mykey spare  # Remove 'mykey' and 'spare' from the private key storage
+
+Aliases: keypairs:private:rm, keypairs:private:del
       DESC
       define_method "keypairs:private:remove" do |name, *names|
         cli_command(options) {
