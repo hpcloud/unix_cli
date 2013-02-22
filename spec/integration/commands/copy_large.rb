e39ad33b9b39674a1bbe969ff85f3fb90ee973d1
howete <thowe@hp.com>
2013-02-21 20:08:59 -0700

    dxcli-668 add automated large file tests

--- spec/integration/commands/copy_large.rb
+++ spec/integration/commands/copy_large.rb
@@ -1,41 +1,121 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "Copy large" do
+class LargeHelper
   @@chunk_size = 200
+
+  def self.chunk_size
+    return @@chunk_size
+  end
+
+  def self.build_body(siz)
+    cnt = 0
+    ray = []
+    chars = ('a'..'z').to_a
+    rand = Random.new
+    while cnt < siz do
+      ray << chars[rand.rand(0..25)]
+      cnt += 1
+    end
+    return ray.join('')
+  end
+
+  def self.build_file(filename, siz)
+    fname = 'spec/tmp/' + filename
+    file = File.new(fname, 'w+')
+    body = LargeHelper.build_body(siz)
+    file.write(body)
+    file.close
+    return fname, body
+  end
+
+  def self.verify_body(filename, body)
+    download = 'spec/tmp/download'
+    FileUtils.rm_f(download)
+    rsp = cptr("copy :large/#{filename} spec/tmp/download")
+    file = File.new(download)
+    result = file.read
+    return result == body
+  end
+end
+
+describe "Copy large" do
 
   before(:all) do
     ConfigHelper.use_tmp()
-    config = Config.new
-    config.set(:storage_chunk_size, @@chunk_size)
+    config = HP::Cloud::Config.new
+    config.set(:storage_chunk_size, LargeHelper.chunk_size)
     config.write
+  end
+
+  before(:each) do
+    cptr('remove -f :large')
+    rsp = cptr('containers:add -f :large')
+    rsp.stderr.should eq("")
   end
 
   context "Large file copy one under" do
     it "should copy file" do
-      rsp = cptr('copy :cross :duplicate')
+      filename = 'oneunder'
+      siz = (LargeHelper.chunk_size - 1)
+      fname, body = LargeHelper.build_file(filename, siz)
+
+      rsp = cptr("copy #{fname} :large")
 
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("Copied :cross => :duplicate\n")
+      rsp.stdout.should eq("Copied #{fname} => :large\n")
       rsp.exit_status.should be_exit(:success)
-      rsp = cptr('list :duplicate')
+      rsp = cptr('list :large')
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("Yeltsin/Boris.txt\nYeltsin/Gorbachev/Andropov.txt\nYeltsin/Gorbachev/Chernenko.txt\nYeltsin/Gorbachev/Mikhail.txt\n")
+      rsp.stdout.should eq(filename + "\n")
+      LargeHelper.verify_body(filename, body).should be_true
     end
   end
     
-  context "when migrate" do
-    it "should copy files" do
-      rsp = cptr('migrate secondary :cross :migrate')
+  context "Large file copy exactly" do
+    it "should copy file" do
+      filename = 'exactly'
+      siz = LargeHelper.chunk_size
+      fname, body = LargeHelper.build_file(filename, siz)
+
+      rsp = cptr("copy #{fname} :large")
 
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("Migrated :cross => :migrate\n")
+      rsp.stdout.should eq("Copied #{fname} => :large\n")
       rsp.exit_status.should be_exit(:success)
-      rsp = cptr('list :migrate')
-      rsp.stderr.should eq("")
-      rsp.stdout.should eq("Yeltsin/Boris.txt\nYeltsin/Gorbachev/Andropov.txt\nYeltsin/Gorbachev/Chernenko.txt\nYeltsin/Gorbachev/Mikhail.txt\n")
+      LargeHelper.verify_body(filename, body).should be_true
     end
   end
+    
+  context "Large file copy one over" do
+    it "should copy file" do
+      filename = 'oneover'
+      siz = (LargeHelper.chunk_size + 1)
+      fname, body = LargeHelper.build_file(filename, siz)
 
+      rsp = cptr("copy #{fname} :large")
+
+      rsp.stderr.should eq("")
+      rsp.stdout.should eq("Copied #{fname} => :large\n")
+      rsp.exit_status.should be_exit(:success)
+      LargeHelper.verify_body(filename, body).should be_true
+    end
+  end
+    
+  context "Large file copy many over" do
+    it "should copy file" do
+      filename = 'manyover'
+      siz = (LargeHelper.chunk_size*10 + 11)
+      fname, body = LargeHelper.build_file(filename, siz)
+
+      rsp = cptr("copy #{fname} :large")
+
+      rsp.stderr.should eq("")
+      rsp.stdout.should eq("Copied #{fname} => :large\n")
+      rsp.exit_status.should be_exit(:success)
+      LargeHelper.verify_body(filename, body).should be_true
+    end
+  end
+    
   after(:all) do
     ConfigHelper.reset()
   end
