7fc3f509ed3e5ad3a95e6b391cc8f77767bc10f0
howete <thowe@hp.com>
2013-02-25 09:38:45 -0700

    dxcli-171 make input and output block size the same

--- spec/perf/makefile.sh
+++ spec/perf/makefile.sh
@@ -9,7 +9,7 @@ SIZE="${2}"
 
 if [ ! -f "${FILE}" ]
 then
-  dd if=/dev/urandom of="${FILE}" ibs=1024 count=${SIZE} >/dev/null 2>/dev/null || exit 1
+  dd if=/dev/urandom of="${FILE}" bs=1024 count=${SIZE} >/dev/null 2>/dev/null || exit 1
 fi
 md5sum -b "${FILE}" | cut -f1 -d' '
 exit 0
