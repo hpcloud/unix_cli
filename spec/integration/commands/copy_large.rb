926041a63979997ac73fd74042d16dd52c3c71b6
howete <thowe@hp.com>
2013-03-18 07:35:26 -0600

    dxcli-714 made the copy large test a little more flexible

--- spec/integration/commands/copy_large.rb
+++ spec/integration/commands/copy_large.rb
@@ -1,7 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 class LargeHelper
-  @@chunk_size = 200
+  @@segment_size = 1024
+  @@chunk_size = 512
+
+  def self.segment_size
+    return @@segment_size
+  end
 
   def self.chunk_size
     return @@chunk_size
@@ -43,8 +48,9 @@ describe "Copy large" do
   before(:all) do
     ConfigHelper.use_tmp()
     config = HP::Cloud::Config.new
-    config.set(:storage_segment_size, LargeHelper.chunk_size)
-    config.set(:storage_max_size, LargeHelper.chunk_size)
+    config.set(:storage_segment_size, LargeHelper.segment_size)
+    config.set(:storage_max_size, LargeHelper.segment_size)
+    config.set(:storage_chunk_size, LargeHelper.chunk_size)
     config.write
   end
 
@@ -56,7 +62,7 @@ describe "Copy large" do
   context "Large file copy one under" do
     it "should copy file" do
       filename = 'oneunder'
-      siz = (LargeHelper.chunk_size - 1)
+      siz = (LargeHelper.segment_size - 1)
       fname, body = LargeHelper.build_file(filename, siz)
 
       rsp = cptr("copy #{fname} :largetest")
@@ -66,7 +72,8 @@ describe "Copy large" do
       rsp.exit_status.should be_exit(:success)
       rsp = cptr('list :largetest -c sname,size')
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("oneunder 199\n")
+      siz = (LargeHelper.segment_size) - 1
+      rsp.stdout.should eq("oneunder #{siz}\n")
       LargeHelper.verify_body(filename, body).should be_true
     end
   end
@@ -74,7 +81,7 @@ describe "Copy large" do
   context "Large file copy exactly" do
     it "should copy file" do
       filename = 'exactly'
-      siz = LargeHelper.chunk_size
+      siz = LargeHelper.segment_size
       fname, body = LargeHelper.build_file(filename, siz)
 
       rsp = cptr("copy #{fname} :largetest")
@@ -84,7 +91,7 @@ describe "Copy large" do
       rsp.exit_status.should be_exit(:success)
       rsp = cptr('list :largetest -c sname,size')
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("exactly 200\n")
+      rsp.stdout.should eq("exactly #{LargeHelper.segment_size}\n")
       LargeHelper.verify_body(filename, body).should be_true
     end
   end
@@ -92,7 +99,7 @@ describe "Copy large" do
   context "Large file copy one over" do
     it "should copy file" do
       filename = 'oneover'
-      siz = (LargeHelper.chunk_size + 1)
+      siz = (LargeHelper.segment_size + 1)
       fname, body = LargeHelper.build_file(filename, siz)
 
       rsp = cptr("copy #{fname} :largetest")
@@ -102,7 +109,7 @@ describe "Copy large" do
       rsp.exit_status.should be_exit(:success)
       rsp = cptr('list :largetest -c sname,size')
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("oneover 0\noneover.segment.0000000001 200\noneover.segment.0000000002 1\n")
+      rsp.stdout.should eq("oneover 0\noneover.segment.0000000001 #{LargeHelper.segment_size}\noneover.segment.0000000002 1\n")
       LargeHelper.verify_body(filename, body).should be_true
     end
   end
@@ -110,7 +117,7 @@ describe "Copy large" do
   context "Large file copy many over" do
     it "should copy file" do
       filename = 'manyover'
-      siz = (LargeHelper.chunk_size*10 + 11)
+      siz = (LargeHelper.segment_size*10 + 11)
       fname, body = LargeHelper.build_file(filename, siz)
 
       rsp = cptr("copy #{fname} :largetest")
@@ -120,7 +127,7 @@ describe "Copy large" do
       rsp.exit_status.should be_exit(:success)
       rsp = cptr('list :largetest -c sname,size')
       rsp.stderr.should eq("")
-      rsp.stdout.should eq("manyover 0\nmanyover.segment.0000000001 200\nmanyover.segment.0000000002 200\nmanyover.segment.0000000003 200\nmanyover.segment.0000000004 200\nmanyover.segment.0000000005 200\nmanyover.segment.0000000006 200\nmanyover.segment.0000000007 200\nmanyover.segment.0000000008 200\nmanyover.segment.0000000009 200\nmanyover.segment.0000000010 200\nmanyover.segment.0000000011 11\n")
+      rsp.stdout.should eq("manyover 0\nmanyover.segment.0000000001 #{LargeHelper.segment_size}\nmanyover.segment.0000000002 #{LargeHelper.segment_size}\nmanyover.segment.0000000003 #{LargeHelper.segment_size}\nmanyover.segment.0000000004 #{LargeHelper.segment_size}\nmanyover.segment.0000000005 #{LargeHelper.segment_size}\nmanyover.segment.0000000006 #{LargeHelper.segment_size}\nmanyover.segment.0000000007 #{LargeHelper.segment_size}\nmanyover.segment.0000000008 #{LargeHelper.segment_size}\nmanyover.segment.0000000009 #{LargeHelper.segment_size}\nmanyover.segment.0000000010 #{LargeHelper.segment_size}\nmanyover.segment.0000000011 11\n")
       LargeHelper.verify_body(filename, body).should be_true
     end
   end
