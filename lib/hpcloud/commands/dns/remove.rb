4ec30ae1cee4171dc8779be38511d348682010c6
Douglas Moran <douglas.moran@hp.com>
2013-05-06 11:39:37 -0500

    Update lib/hpcloud/commands/dns/remove.rb

--- lib/hpcloud/commands/dns/remove.rb
+++ lib/hpcloud/commands/dns/remove.rb
@@ -4,14 +4,14 @@ module HP
 
       map %w(dns:rm dns:delete dns:del) => 'dns:remove'
 
-      desc "dns:remove name_or_id [name_or_id ...]", "Remove dns domains (specified by name or ID)."
+      desc "dns:remove name_or_id [name_or_id ...]", "Remove DNS domains (specified by name or ID)."
       long_desc <<-DESC
-  Remove dns domains by specifying their names or ID. You may specify more than one dns name or ID on a command line.  Optionally, you can specify an availability zone.
+  Remove DNS domains by specifying their names or ID. You may specify more than one DNS name or ID on a command line.  Optionally, you can specify an availability zone.
 
 Examples:
-  hpcloud dns:remove tome treatise   # Delete the dns 'tome' and 'treatise':
-  hpcloud dns:remove 998             # Delete the dns with ID 998:
-  hpcloud dns:remove my-dns -z az-2.region-a.geo-1  # Delete the dns `my-dns` for availability zone `az-2.region-a.geo-1`:
+  hpcloud dns:remove tome treatise   # Delete the DNS domains `tome` and `treatise`:
+  hpcloud dns:remove 998             # Delete the DNS domain with ID 998:
+  hpcloud dns:remove my-dns -z az-2.region-a.geo-1  # Delete the DNS domain `my-dns` for availability zone `az-2.region-a.geo-1`:
 
 Aliases: dns:rm, dns:delete, dns:del
       DESC
