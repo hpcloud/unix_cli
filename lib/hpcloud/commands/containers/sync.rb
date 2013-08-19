8bba12c3d9338e5b06d71cb4185b2230e35811ad
Terry Howe <thowe@hp.com>
2013-08-19 09:23:57 -0600

    dxcli-972 fix help text

--- lib/hpcloud/commands/containers/sync.rb
+++ lib/hpcloud/commands/containers/sync.rb
@@ -4,12 +4,12 @@ module HP
     
       desc "containers:sync name key [location]", "Allow container synchronization."
       long_desc <<-DESC
-  Allow container synchronization using the specified key.  If you are creating a destination for synchronization, only the key should be specified.  If you are creating a source for synchronization, specify a key and location.  The same key must be used in the source and destination.  It is possible have containers as both a source and destination.  List your synchronization information with the "hpcloud list --sync" command.
+  Allow container synchronization using the specified key.  If you are creating a destination for synchronization, only the key should be specified.  If you are creating a source for synchronization, specify a key and location.  The same key must be used in the source and destination.  It is possible to have containers as both a source and destination.  List your synchronization information with the "hpcloud list --sync" command.
 
 Examples:
   hpcloud containers:sync :atainer keyo         # Set up the container :atainer to be a destination for synchronization
   hpcloud containers:sync :btainer keyo atainer # Synchronize :btainer to remote container :atainer
-  hpcloud containers:sync :atainer keyo https://region-b.geo-1.objects.hpcloudsvc.com:443/v1/96XXXXXX/btainer     # Create a two way synchronization betwee :atainer and :btainer
+  hpcloud containers:sync :atainer keyo https://region-b.geo-1.objects.hpcloudsvc.com:443/v1/96XXXXXX/btainer     # Create a two way synchronization between :atainer and :btainer
       DESC
       CLI.add_common_options
       define_method "containers:sync" do |name, key, *location|
