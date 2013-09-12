9e7a0e48c3e75ecc794f4e7a6ee3766a5c859284
Terry Howe <thowe@hp.com>
2013-09-12 11:38:55 -0600

    add network

--- spec/helpers/server_test_helper.rb
+++ spec/helpers/server_test_helper.rb
@@ -13,9 +13,10 @@ class ServerTestHelper
     end
     server = servers.create()
     server.name = name
-    server.flavor = AccountsHelper.get_flavor_id()
+    server.flavor = AccountsHelper.get_flavor_id().to_i
     server.image = AccountsHelper.get_image_id()
     server.set_keypair(keypair.name)
+    server.set_network('cli_test_network1')
     server.save
     server.fog.wait_for { ready? }
     @@server_cache[name] = server
