0aa758f9da1a7d3af6a6a3baaac10ebc4194b64b
Terry Howe <thowe@hp.com>
2013-09-17 08:56:36 -0600

    fix the version test

--- spec/integration/commands/lb/versions_spec.rb
+++ spec/integration/commands/lb/versions_spec.rb
@@ -5,7 +5,7 @@ describe "lb:versions" do
     it "should report success" do
       rsp = cptr("lb:versions -c id,status -d X")
 
-      rsp.stderr.should eq("The following keys are invalid: :version\n")
+      rsp.stderr.should eq("")
       rsp.stdout.should match("v1.1XCURRENT\n")
       rsp.exit_status.should be_exit(:success)
     end
