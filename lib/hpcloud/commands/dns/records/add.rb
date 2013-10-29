67351065da073ef5a2079e907dadc8e378f26a0e
Terry Howe <terrylhowe@gmail.com>
2013-10-29 10:29:09 -0600

    dxcli-1012 add documentation for the priority option

--- lib/hpcloud/commands/dns/records/add.rb
+++ lib/hpcloud/commands/dns/records/add.rb
@@ -2,12 +2,12 @@ module HP
   module Cloud
     class CLI < Thor
 
-      desc "dns:records:add <domain> <name> <type> <data>", "Add a DNS record."
+      desc "dns:records:add <domain> <name> <type> <data> [priority]", "Add a DNS record."
       long_desc <<-DESC
-  Add a DNS record to the specified domain with the given name, type and data.
+  Add a DNS record to the specified domain with the given name, type and data.  Optionally, a priority may be added.
 
 Examples:
-  hpcloud dns:records:add mydomain.com. www.mydomain.com. A 10.0.0.1 # Create a DNS record for domain `mydomain.com` and `A` record for `www.mydomain.com` pointing to address 10.0.0.1
+  hpcloud dns:records:add mydomain.com. www.mydomain.com. A 10.0.0.1 # Create a DNS record for domain `mydomain.com` and `A` record for `www.mydomain.com` pointing to address 10.0.0.1.
       DESC
       CLI.add_common_options
       define_method "dns:records:add" do |domain, name, type, data, *priority|
